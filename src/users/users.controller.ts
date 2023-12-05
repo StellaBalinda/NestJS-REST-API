f2fc7e2bececcab25b4d490a7854656a5f9479e0
StellaBalinda <41113674+StellaBalinda@users.noreply.github.com>
2023-12-05 12:46:01 +0200

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, ValidationPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from 'src/dto/create-user.dto';
+import { UpdateUserDto } from 'src/dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get() 
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') { 
+        return this.usersService.findAll(role);
+    } 
+
+    // @Get('interns')
+    // findAllInterns() {
+    //     return []
+    // }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.findOne(id )
+    }
+
+    @Post()
+    create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto) 
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto: UpdateUserDto) {
+        return this.usersService.update(id, updateUserDto)
+    }
+
+    @Delete(':id')
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.delete(id)
+    }
+    
+}
